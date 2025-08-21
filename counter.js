348461e489ecff12092479d3898be7624b8581d4
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2025-08-21 15:57:32 -0700

    reorder lines

--- counter.js
+++ counter.js
@@ -1,8 +1,8 @@
 export function counter({}, realm = {a: "Model", change: "Model"}) {
-  const {h, html, render} = import("./preact.standalone.module.js");
   const change = Events.or(incr, decr);
   const a = Behaviors.collect(0, change, (prev, change) => prev + change);
 
+  const {h, html, render} = import("./preact.standalone.module.js");
   const incr = Events.listener(document.body.querySelector("#incr"), "click", (evt) => 1);
   const decr = Events.listener(document.body.querySelector("#decr"), "click", (evt) => -1);
 
@@ -11,4 +11,3 @@ export function counter({}, realm = {a: "Model", change: "Model"}) {
 
   return {};
 }
-
