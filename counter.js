de3588f9aecdcab45040395aaec287450411a15d
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2025-08-21 16:21:34 -0700

    let us make it one line shorter

--- counter.js
+++ counter.js
@@ -1,13 +1,12 @@
 export function counter({}, realm = {a: "Model", change: "Model"}) {
-  const change = Events.or(incr, decr);
+  const timer = Events.timer(1000);
+  const change = Events.or(incr, decr, timer);
   const a = Behaviors.collect(0, change, (prev, change) => prev + change);
 
-  const {h, html, render} = import("./preact.standalone.module.js");
+  const {html, render} = import("./preact.standalone.module.js");
   const incr = Events.listener(document.body.querySelector("#incr"), "click", (evt) => 1);
   const decr = Events.listener(document.body.querySelector("#decr"), "click", (evt) => -1);
-
-  const dom = h("div", {}, a);
-  render(dom, document.body.querySelector("#count"));
+  render(html`<div>${a}</div>`, document.body.querySelector("#count"));
 
   return {};
 }
